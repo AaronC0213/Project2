42b41f95e637e9b59c014bc2e075f0c39f98bfe5
User <User@DESKTOP-08Q4BG6.attlocal.net>
2023-04-05 22:28:47 -0400

    Initial Commit

---
+++ src/project_2Demo/Project2_Test.java
@@ -0,0 +1,12 @@
+package project_2Demo;
+
+public class Project2_Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("test 2...");
+		
+		System.out.println("Hello team");
+	}
+
+}
